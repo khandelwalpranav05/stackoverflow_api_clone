625544806ecb972a69156b8eec0c341fb2a832a5
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-06-06 17:33:29 +0530

    question,answers list view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .serializers import QuestionSerializer,AnswerSerializer
+from .models import Question,Answer
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import viewsets
 
 # Create your views here.
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    serializer_class = QuestionSerializer
+    queryset = Question.objects.all()
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    serializer_class = AnswerSerializer
+    queryset = Answer.objects.all()
